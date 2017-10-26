2a0446d7e134658aa9ed3d6fac676e0c310b2962
Todd Baert <toddbaert@gmail.com>
2017-10-26 11:32:36 -0400

    removing springservletintializer

--- camel-springboot-rest/src/main/java/org/outsrcd/sample/Application.java
+++ camel-springboot-rest/src/main/java/org/outsrcd/sample/Application.java
@@ -2,14 +2,13 @@ package org.outsrcd.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 @ComponentScan
-public class Application extends SpringBootServletInitializer{
+public class Application {
 
     // must have a main method spring-boot can run
     public static void main(String[] args) {
